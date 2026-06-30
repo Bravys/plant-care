b7aa9ac04d51f05cdcc0cf46df0639f3a64aa3a0
Bravys <aadisankarv2011@gmail.com>
2026-06-30 22:48:01 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,36 @@
+const CACHE_NAME = "plant-care-cache-v1";
+const urlsToCache = [
+  "index.html",
+  "style.css",
+  "app.js",
+  "manifest.json"
+];
+
+// Install service worker and cache files
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Fetch cached files
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+// Update cache
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      );
+    })
+  );
+});
